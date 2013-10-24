59a4a77f6891fd66ba6e91be704fa9901242098f
Tim Cull <trcull@thedwick.com>
2013-10-23 22:06:12 -0700

    got a nice UI working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RetentionfactoryEngine::Application.routes.draw do
-  root 'application#index'
+  root 'splash#index'
 
   devise_for :users, :controllers => {:sessions => 'sessions', :registrations=>'registrations'} #override the default devise controller with our own in app/controllers/sessions_controller
 
